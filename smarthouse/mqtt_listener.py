d1dbcd82eb33000d9f3d834ec0a37c5a15dabe7e
islis <77433540+islis@users.noreply.github.com>
2021-08-25 20:18:48 +0500

    Delete mqtt_listener.py

--- smarthouse/mqtt_listener.py
+++
@@ -1,8 +0,0 @@
-# import django
-# from django.conf import settings
-# settings.configure()
-# django.setup()
-# from smarthouse.test_mqtt import listener
-#
-# if __name__ == '__main__':
-#     listener()
